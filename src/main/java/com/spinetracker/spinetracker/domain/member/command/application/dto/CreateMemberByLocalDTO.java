13db782d906ee635089f521b1c065c1a06c895c0
hyojeong nam <namgywjd2@gmail.com>
2023-09-10 14:22:51 +0900

    [Close #14] Feat : CREATE APPLICATION SERVICE TEST 코드 구현

--- src/main/java/com/spinetracker/spinetracker/domain/member/command/application/dto/CreateMemberByLocalDTO.java
+++ src/main/java/com/spinetracker/spinetracker/domain/member/command/application/dto/CreateMemberByLocalDTO.java
@@ -1,22 +1,38 @@
 package com.spinetracker.spinetracker.domain.member.command.application.dto;
 
-import com.spinetracker.spinetracker.domain.member.command.domain.aggregate.entity.enumtype.RoleEnum;
-import com.spinetracker.spinetracker.domain.member.command.domain.aggregate.entity.vo.MemberInfoVO;
 import lombok.Getter;
 import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @ToString
 public class CreateMemberByLocalDTO {
-    private final String email;
-    private final String password;
-    private final RoleEnum role;
-    private final MemberInfoVO memberInfo;
 
-    public CreateMemberByLocalDTO(String email, String password, RoleEnum role, MemberInfoVO memberInfo) {
+    @Email(message = "NOT_VALID_EMAIL")
+    private final String email;
+    @NotBlank(message = "PASSWORD_IS_MANDATORY")
+    @NotNull
+    private final String password;
+    private final String role;
+    @NotBlank(message = "NAME_IS_NOT_BLANK")
+    private final String name;
+    @NotBlank(message = "GENDER_IS_NOT_BLANK")
+    private final String gender;
+    @NotBlank(message = "AGERANGE_IS_NOT_BLANK")
+    private final String ageRange;
+    @NotBlank(message = "JOB_IS_NOT_BLANK")
+    private final String job;
+
+    public CreateMemberByLocalDTO(String email, String password, String name, String gender, String ageRange, String job) {
         this.email = email;
         this.password = password;
-        this.role = role;
-        this.memberInfo = memberInfo;
+        this.role = "MEMBER";
+        this.name = name;
+        this.gender = gender;
+        this.ageRange = ageRange;
+        this.job = job;
     }
 }
--- src/main/java/com/spinetracker/spinetracker/domain/member/command/application/service/CreateMemberService.java
+++ src/main/java/com/spinetracker/spinetracker/domain/member/command/application/service/CreateMemberService.java
@@ -3,9 +3,14 @@ package com.spinetracker.spinetracker.domain.member.command.application.service;
 import com.spinetracker.spinetracker.domain.member.command.application.dto.CreateMemberByLocalDTO;
 import com.spinetracker.spinetracker.domain.member.command.application.dto.CreateMemberBySocialDTO;
 import com.spinetracker.spinetracker.domain.member.command.domain.aggregate.entity.Member;
+import com.spinetracker.spinetracker.domain.member.command.domain.aggregate.entity.enumtype.AgeRangeEnum;
+import com.spinetracker.spinetracker.domain.member.command.domain.aggregate.entity.enumtype.GenderEnum;
+import com.spinetracker.spinetracker.domain.member.command.domain.aggregate.entity.enumtype.RoleEnum;
+import com.spinetracker.spinetracker.domain.member.command.domain.aggregate.entity.vo.MemberInfoVO;
 import com.spinetracker.spinetracker.domain.member.command.domain.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CreateMemberService {
@@ -18,12 +23,24 @@ public class CreateMemberService {
     }
 
     // 자체 로그인 시
+    @Transactional
     public Member createMemberByLocal(CreateMemberByLocalDTO createMemberByLocalDTO) {
+
+        GenderEnum memberGender = GenderEnum.valueOf(createMemberByLocalDTO.getGender());
+        AgeRangeEnum memberAgeRange = AgeRangeEnum.valueOf(createMemberByLocalDTO.getAgeRange());
+        MemberInfoVO newMemberInfoVO = new MemberInfoVO(
+                createMemberByLocalDTO.getName(),
+                memberGender,
+                memberAgeRange,
+                createMemberByLocalDTO.getJob()
+        );
+
+        RoleEnum memberRole = RoleEnum.valueOf(createMemberByLocalDTO.getRole());
         Member createdMemberByLocal = new Member(
                 createMemberByLocalDTO.getEmail(),
                 createMemberByLocalDTO.getPassword(),
-                createMemberByLocalDTO.getRole(),
-                createMemberByLocalDTO.getMemberInfo()
+                memberRole,
+                newMemberInfoVO
         );
         return memberRepository.save(createdMemberByLocal);
     }
